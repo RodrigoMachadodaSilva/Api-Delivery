d6606e946901f22f5a705b1afc3e7e5f8a0e6701
RodrigoMachadodaSilva <87843482+RodrigoMachadodaSilva@users.noreply.github.com>
2023-05-04 00:42:58 -0300

    Finalização do projeto, inclusão de documentação com Spring Doce segurança com Spring Security

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -2,53 +2,49 @@ package com.algaworks.algafood.api.exceptionhandler;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.OffsetDateTime;
 import java.util.List;
 
-@ApiModel("Problema")
 @JsonInclude(Include.NON_NULL)
-@Getter
 @Builder
+@Getter
+@Schema(name = "Problema")
 public class Problem {
 
-	@ApiModelProperty(example = "400", position = 1)
+	@Schema(example = "400")
 	private Integer status;
 
-	@ApiModelProperty(example = "2019-12-01T18:09:02.70844Z", position = 5)
-	private OffsetDateTime timestamp;
-
-	@ApiModelProperty(example = "https://algafood.com.br/dados-invalidos", position = 10)
+	@Schema(example = "https://algafood.com.br/dados-invalidos")
 	private String type;
 
-	@ApiModelProperty(example = "Dados inválidos", position = 15)
+	@Schema(example = "Dados inválidos")
 	private String title;
 
-	@ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.",
-			position = 20)
+	@Schema(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
 	private String detail;
 
-	@ApiModelProperty(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.",
-			position = 25)
+	@Schema(example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.")
 	private String userMessage;
 
-	@ApiModelProperty(value = "Lista de objetos ou campos que geraram o erro (opcional)",
-			position = 30)
+	@Schema(example = "2022-07-15T11:21:50.902245498Z")
+	private OffsetDateTime timestamp;
+
+	@Schema(description = "Lista de objetos ou campos que geraram o erro")
 	private List<Object> objects;
 
-	@ApiModel("ObjetoProblema")
-	@Getter
 	@Builder
+	@Getter
+	@Schema(name = "ObjetoProblema")
 	public static class Object {
 
-		@ApiModelProperty(example = "preco")
+		@Schema(example = "preco")
 		private String name;
 
-		@ApiModelProperty(example = "O preço é obrigatório")
+		@Schema(example = "O preço é inválido")
 		private String userMessage;
 
 	}
